68208ccbca37ce51ee0870730bfd68140c8199bf
Ashwini Visave <ashwini.visave@nepa.com>
2025-05-24 22:51:22 +0200

    Demo of using LLM model with langchain

---
+++ 1.LLMs/1_llm_demo.py
@@ -0,0 +1,14 @@
+## Import necessary libraries
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+llm = OpenAI(model="gpt-3.5-turbo-instruct")
+
+result = llm.invoke("What is the capital of India?")
+
+print(result)
+
